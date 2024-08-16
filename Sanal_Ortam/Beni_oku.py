5a328814aad81e793a6c440c070351d4a460e89a
Kuzey T. Kale <166996695+kuzeytunakale@users.noreply.github.com>
2024-08-16 22:43:11 +0300

    Add files via upload

---
+++ Sanal_Ortam/Beni_oku.py
@@ -0,0 +1,7 @@
+#import subprocess
+#subprocess.run(["pip", "install", "virtualenv"])
+
+#Sanal oetam yaratmak için: python -m venv sanalortamadı
+#(Genelde bunu yap)Var olan sanal ortamı çalıştırmak için: .\sanalortamadı\Scripts\activate
+#Var olan sanal ortamı çalıştırmak için: C:\Users\Kuzey Tuna\Desktop\VSCode\Sanal_ortam\kuzey\Scripts\activate
+#Sanal ortamdan çıkmak için: deactivate
